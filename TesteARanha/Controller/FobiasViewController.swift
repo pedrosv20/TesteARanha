4577ce6041f29885be8a3779cd2a00399763c50a
Maria Fernanda Azolin <azolin.mf@gmail.com>
2019-10-16 11:50:53 -0300

    add table view to first screen (select phobias)

--- TesteARanha/Controller/FobiasViewController.swift
+++ TesteARanha/Controller/FobiasViewController.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
-class FobiasViewController: UIViewController {
-
+class FobiasViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    @IBOutlet weak var phobiasTableView: UITableView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+        
+        phobiasTableView?.delegate = self
+        phobiasTableView?.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fobiaCell", for: indexPath) as! FobiaCell
+        
+        cell.phobiaLabel.text = "ARANHAS"
+        cell.backgroundImage.image = UIImage(named: "spidersBackground")
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "niveis") as? NiveisViewController {
+            vc.selectedPhobia = indexPath.row
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
+    
 }
---
+++ TesteARanha/Model/FobiaCell.swift
@@ -0,0 +1,16 @@
+//
+//  FobiaCell.swift
+//  TesteARanha
+//
+//  Created by Maria Fernanda Azolin on 16/10/19.
+//  Copyright © 2019 Pedro Vargas. All rights reserved.
+//
+
+import UIKit
+
+class FobiaCell: UITableViewCell {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var phobiaLabel: UILabel!
+    
+}
