996d3b96b2ec66154540cee596de1a81f61a1685
Maria Fernanda Azolin <azolin.mf@gmail.com>
2019-10-21 11:34:57 -0300

    start editing Text Level viewController

--- TesteARanha/Controller/Nivel1ViewController.swift
+++ TesteARanha/Controller/Nivel1ViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class Nivel1ViewController: UIViewController {
 
+    
+    @IBOutlet weak var boardImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.boardImageView.layer.masksToBounds = true
+        self.boardImageView.layer.cornerRadius = self.boardImageView.frame.width/9.0
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
