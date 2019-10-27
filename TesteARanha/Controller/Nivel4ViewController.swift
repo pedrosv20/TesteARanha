67ac7a221411665188666b238a579c4ff9434673
Pedro <pedro1silva1vargas@gmail.com>
2019-10-27 16:42:45 -0300

    Add RealityKit project in etapa4ViewController

--- TesteARanha/Controller/Nivel4ViewController.swift
+++ TesteARanha/Controller/Nivel4ViewController.swift
@@ -1,142 +1,23 @@
 import UIKit
-import ARKit
+import RealityKit
 
 class Nivel4ViewController: UIViewController {
     
-    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet var arView: ARView!
+    var anchor: Aranha.Cena!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        anchor = try! Aranha.loadCena()
+        anchor.generateCollisionShapes(recursive: true)
+        // Load the "Box" scene from the "Experience" Reality File
+        arView.scene.anchors.append(anchor)
         
-        addTapGestureToSceneView()
-        
-        configureLighting()
+        // Add the box anchor to the scene
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setUpSceneView()
-        self.navigationController?.navigationBar.isHidden = true
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        sceneView.session.pause()
-    }
-    
-    func setUpSceneView() {
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = .horizontal
-        
-        sceneView.session.run(configuration)
-        
-        sceneView.delegate = self as! ARSCNViewDelegate
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-    }
-    
-    func configureLighting() {
-        sceneView.autoenablesDefaultLighting = true
-        sceneView.automaticallyUpdatesLighting = true
-    }
-    
-    @objc func addShipToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer) {
-        let tapLocation = recognizer.location(in: sceneView)
-        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
-        
-        guard let hitTestResult = hitTestResults.first else { return }
-        let translation = hitTestResult.worldTransform.translation
-        let x = translation.x
-        let y = translation.y
-        let z = translation.z
-        
-        guard let shipScene = SCNScene(named: "spider.dae"),
-            
-            let shipNode = shipScene.rootNode.childNode(withName: "spider", recursively: false)
-            
-            
-            else { return }
-        shipNode.name = "spider"
-        
-        shipNode.position = SCNVector3(x,y,z)
-        shipNode.scale = SCNVector3(0.005, 0.005, 0.005)
-        sceneView.scene.rootNode.addChildNode(shipNode)
-    }
-    
-    
-    
-    func addTapGestureToSceneView() {
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(Nivel4ViewController.addShipToSceneView(withGestureRecognizer:)))
-        sceneView.addGestureRecognizer(tapGestureRecognizer)
-    }
 }
 
-extension float4x4 {
-    var translation: SIMD3<Float> {
-        let translation = self.columns.3
-        return SIMD3<Float>(translation.x, translation.y, translation.z)
-    }
-}
 
-extension UIColor {
-    open class var transparentLightBlue: UIColor {
-        return UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 0.50)
-    }
-}
-
-extension Nivel4ViewController: ARSCNViewDelegate {
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        // 1
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
-        // 2
-        let width = CGFloat(planeAnchor.extent.x)
-        let height = CGFloat(planeAnchor.extent.z)
-        let plane = SCNPlane(width: width, height: height)
-        
-        // 3
-        plane.materials.first?.diffuse.contents = UIColor.transparentLightBlue
-        
-        // 4
-        let planeNode = SCNNode(geometry: plane)
-        
-        // 5
-        let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
-        let z = CGFloat(planeAnchor.center.z)
-        planeNode.position = SCNVector3(x,y,z)
-        planeNode.eulerAngles.x = -.pi / 2
-        
-        // 6
-        node.addChildNode(planeNode)
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        // 1
-        guard let planeAnchor = anchor as?  ARPlaneAnchor,
-            let planeNode = node.childNodes.first,
-            let plane = planeNode.geometry as? SCNPlane
-            else { return }
-        
-        // 2
-        let width = CGFloat(planeAnchor.extent.x)
-        let height = CGFloat(planeAnchor.extent.z)
-        plane.width = width
-        plane.height = height
-        
-        // 3
-        let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
-        let z = CGFloat(planeAnchor.center.z)
-        planeNode.position = SCNVector3(x, y, z)
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        print(sceneView.scene.rootNode.childNodes.count)
-        if let spider = sceneView.scene.rootNode.childNode(withName: "spider", recursively: false) {
-            print(spider.position.x)
-//            spider.position.x += 0.005
-        }
-        
-    }
-}
