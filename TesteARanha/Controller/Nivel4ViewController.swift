4d44b47f489316dbf9f1f09e5fed6899e4043ce1
Pedro <pedro1silva1vargas@gmail.com>
2019-10-31 11:31:41 -0300

    Coach Working and Adding spider

--- TesteARanha/Controller/Nivel4ViewController.swift
+++ TesteARanha/Controller/Nivel4ViewController.swift
@@ -3,13 +3,13 @@ import RealityKit
 import ARKit
 
 class Nivel4ViewController: UIViewController, UICollectionViewDelegate, ARCoachingOverlayViewDelegate {
-
+    
     @IBOutlet var arView: ARView!
     var entity : Entity!
     var anchor: AnchorEntity!
     
     var cellIds = ["aa", "bb", "cc", "dd", "ee"]
-    let cellSizes = Array( repeatElement(CGSize(width:414, height:171), count: 5))
+    var cellSizes = Array( repeatElement(CGSize(width:414, height:171), count: 4))
     
     var coachQuantico = ARCoachingOverlayView()
     
@@ -20,6 +20,7 @@ class Nivel4ViewController: UIViewController, UICollectionViewDelegate, ARCoachi
     
     override func viewWillAppear(_ animated: Bool) {
         setOverlay(automatically: true, forDetectionType: .horizontalPlane)
+        //TODO: chamar set overlay quando der swipe pra terceira collection
     }
     
     func setOverlay(automatically: Bool, forDetectionType goal: ARCoachingOverlayView.Goal){
@@ -41,11 +42,12 @@ class Nivel4ViewController: UIViewController, UICollectionViewDelegate, ARCoachi
         coachQuantico.translatesAutoresizingMaskIntoConstraints = false
         
         //3. Enable The Overlay To Activate Automatically Based On User Preference
-        coachQuantico.setActive(true, animated: true)
+        coachQuantico.setActive(true, animated: false)
         print("goal")
         //4. Set The Purpose Of The Overlay Based On The User Preference
         self.coachQuantico.goal = goal
-        coachQuantico.activatesAutomatically = false
+        
+        coachQuantico.activatesAutomatically = true
         
     }
     
@@ -56,19 +58,20 @@ class Nivel4ViewController: UIViewController, UICollectionViewDelegate, ARCoachi
     
     //2. Called When The ARCoachingOverlayView Is No Active And No Longer Displayer
     public func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
-                anchor = AnchorEntity(plane: .horizontal)
-                arView.scene.addAnchor(anchor)
-                let url = Bundle.main.url(forResource: "spiderAnimated.usdz", withExtension: nil)
-                entity = try? Entity.loadModel(contentsOf: url!)
-                anchor.addChild(entity!)
-                entity.stopAllAnimations()
+        anchor = AnchorEntity(plane: .horizontal)
+        arView.scene.addAnchor(anchor)
+        let url = Bundle.main.url(forResource: "spiderAnimated.usdz", withExtension: nil)
+        entity = try? Entity.loadModel(contentsOf: url!)
+        anchor.addChild(entity!)
+        entity.stopAllAnimations()
+        coachQuantico.activatesAutomatically = false
     }
     
     //3. Called When Tracking Conditions Are Poor Or The Seesion Needs Restarting
     
-//    public func coachingOverlayViewDidRequestSessionReset(_ coachingOverlayView: ARCoachingOverlayView) {
-//
-//    }
+    public func coachingOverlayViewDidRequestSessionReset(_ coachingOverlayView: ARCoachingOverlayView) {
+        
+    }
     
     
     @IBAction func addSpider(_ sender: Any) {
@@ -83,25 +86,28 @@ class Nivel4ViewController: UIViewController, UICollectionViewDelegate, ARCoachi
 
 extension Nivel4ViewController: UICollectionViewDataSource {
     func collectionView( _ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return cellSizes.count
         
     }
     func collectionView( _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
         return collectionView.dequeueReusableCell( withReuseIdentifier: cellIds[indexPath.item], for: indexPath)
     }
 }
 extension Nivel4ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
         return cellSizes[indexPath.item]
     }
 }
 
 
-    
-    
-    
-     
-    
+
+
+
+
+
 
 
 
