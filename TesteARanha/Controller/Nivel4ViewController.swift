0c65d40a103a824fe5cdd2f05c22d33edd8e1d5f
Pedro <pedro1silva1vargas@gmail.com>
2019-10-31 09:37:43 -0300

    CollectionView Adicionada

--- TesteARanha/Controller/Nivel4ViewController.swift
+++ TesteARanha/Controller/Nivel4ViewController.swift
@@ -1,67 +1,51 @@
 import UIKit
 import RealityKit
 
-class Nivel4ViewController: UIViewController {
-    
+class Nivel4ViewController: UIViewController, UICollectionViewDelegate {
+
     @IBOutlet var arView: ARView!
     var entity : Entity!
     var anchor: AnchorEntity!
     
+    var cellIds = ["aa", "bb", "cc", "dd"]
+    let cellSizes = Array( repeatElement(CGSize(width:414, height:171), count: 4))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         anchor = AnchorEntity(plane: .horizontal)
-
         arView.scene.addAnchor(anchor)
-
-        
-        
         let url = Bundle.main.url(forResource: "spiderAnimated.usdz", withExtension: nil)
         entity = try? Entity.loadModel(contentsOf: url!)
         anchor.addChild(entity!)
         entity.stopAllAnimations()
 
-        
     }
-
     
     @IBAction func addSpider(_ sender: Any) {
         entity.stopAllAnimations()
-        
         entity.playAnimation(entity.availableAnimations[1].repeat(count:.max))
-
     }
     
     @IBAction func stopAnimation(_ sender: Any) {
         entity.stopAllAnimations()
-        
     }
 }
-//    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
-//        guard let touchInView = sender?.location(in: arView) else {
-//            return
-//        }
-//
-//        guard let hitEntity = arView.entity(at: touchInView) else {
-//            // no entity was hit
-//            return
-//        }
-//        print(hitEntity.name)
-//
-//    }
-//}
-//
-//// randomScale is an example that gives feedback
-//extension Entity {
-//    func randomScale() {
-//        var newTransform = self.transform
-//        newTransform.scale = .init(
-//            repeating: Float.random(in: 0.05...0.1)
-//        )
-//        self.transform = newTransform
-//    }
-//}
+
+extension Nivel4ViewController: UICollectionViewDataSource {
+    func collectionView( _ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+        
+    }
+    func collectionView( _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell( withReuseIdentifier: cellIds[indexPath.item], for: indexPath)
+    }
+}
+extension Nivel4ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSizes[indexPath.item]
+    }
+}
 
 
 
