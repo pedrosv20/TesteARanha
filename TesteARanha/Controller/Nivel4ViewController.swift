acf8e0e152ae976a0e5447e7a921a1995b15428f
Pedro <pedro1silva1vargas@gmail.com>
2019-10-31 08:38:41 -0300

    SpiderAnimated in realitykit

--- TesteARanha/Controller/Nivel4ViewController.swift
+++ TesteARanha/Controller/Nivel4ViewController.swift
@@ -4,25 +4,65 @@ import RealityKit
 class Nivel4ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
-    var anchor: Aranha.Cena!
+    var entity : Entity!
+    var anchor: AnchorEntity!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        anchor = try! Aranha.loadCena()
-        anchor.generateCollisionShapes(recursive: true)
+
+
+        anchor = AnchorEntity(plane: .horizontal)
+
+        arView.scene.addAnchor(anchor)
+
         
-        // Load the "Box" scene from the "Experience" Reality File
-        arView.scene.anchors.append(anchor)
         
-        // Add the box anchor to the scene
-    
+        let url = Bundle.main.url(forResource: "spiderAnimated.usdz", withExtension: nil)
+        entity = try? Entity.loadModel(contentsOf: url!)
+        anchor.addChild(entity!)
+        entity.stopAllAnimations()
+
+        
     }
+
     
     @IBAction func addSpider(_ sender: Any) {
-        anchor.findEntity(named: "spider")?.isEnabled = false
+        entity.stopAllAnimations()
         
+        entity.playAnimation(entity.availableAnimations[1].repeat(count:.max))
+
     }
     
+    @IBAction func stopAnimation(_ sender: Any) {
+        entity.stopAllAnimations()
+        
+    }
 }
+//    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+//        guard let touchInView = sender?.location(in: arView) else {
+//            return
+//        }
+//
+//        guard let hitEntity = arView.entity(at: touchInView) else {
+//            // no entity was hit
+//            return
+//        }
+//        print(hitEntity.name)
+//
+//    }
+//}
+//
+//// randomScale is an example that gives feedback
+//extension Entity {
+//    func randomScale() {
+//        var newTransform = self.transform
+//        newTransform.scale = .init(
+//            repeating: Float.random(in: 0.05...0.1)
+//        )
+//        self.transform = newTransform
+//    }
+//}
+
+
 
 
