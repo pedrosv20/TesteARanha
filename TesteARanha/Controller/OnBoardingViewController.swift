76ad67e87f38a3a35b6d370f1dc5b9b77f1bbf46
Letícia Pulner <leticia.pulner@gmail.com>
2019-11-06 09:02:16 -0300

    onboarding

---
+++ TesteARanha/Controller/OnBoardingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OnBoardingViewController.swift
+//  TesteARanha
+//
+//  Created by Leticia Pulner on 31/10/19.
+//  Copyright © 2019 Pedro Vargas. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//
+//        return cell
+//    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
