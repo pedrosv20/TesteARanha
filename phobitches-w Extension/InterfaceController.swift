4bc073a53d5f8dabe6273544369c066bd5fdd4d6
Pyettra Ferreira <pyettraf@gmail.com>
2019-11-08 16:50:55 -0300

    WatchOS extension

---
+++ phobitches-w Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  phobitches-w Extension
+//
+//  Created by Pyettra Ferreira on 08/11/19.
+//  Copyright © 2019 Pedro Vargas. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
