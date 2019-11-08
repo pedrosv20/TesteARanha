cae6f9127bae76217ee9f46f42f32a4dba03456b
Pedro <pedro1silva1vargas@gmail.com>
2019-11-08 14:03:03 -0300

    Fix f***** constraints

--- TesteARanha/Controller/OnBoardingViewController.swift
+++ TesteARanha/Controller/OnBoardingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class OnBoardingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class OnBoardingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     var cellIds = ["Onboarding1", "Onboarding2", "Onboarding3", "Onboarding4", "Onboarding5"]
     
@@ -20,6 +20,13 @@ class OnBoardingViewController: UIViewController, UICollectionViewDelegate, UICo
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width:self.view.frame.width, height:self.view.frame.height)
+        
+    }
+    
     
 //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //
